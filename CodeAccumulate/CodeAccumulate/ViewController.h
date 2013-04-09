97c9ee658ce902040740383c28ec2354ac557e76
王金宇 <wangjy_email@163.com>
2013-04-09 14:29:24 +0800

    创建空工程

---
+++ CodeAccumulate/CodeAccumulate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CodeAccumulate
+//
+//  Created by 王金宇 on 13-4-9.
+//  Copyright (c) 2013年 王金宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CodeAccumulate/CodeAccumulate/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CodeAccumulate
+//
+//  Created by 王金宇 on 13-4-9.
+//  Copyright (c) 2013年 王金宇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
